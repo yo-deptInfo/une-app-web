2882c92c1103ddb6c1f43868971310d46290d631
yo <ledicodeyo@gmail.com>
2025-05-06 16:00:26 -0400

    avec les imports svp

--- src/main/java/cal/demo/UneAppWeb.java
+++ src/main/java/cal/demo/UneAppWeb.java
@@ -1,7 +1,12 @@
 package cal.demo;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 
 @WebServlet("/demo")
 public class UneAppWeb extends HttpServlet {
